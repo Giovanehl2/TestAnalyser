3ce6b0b4688d6f89ac2f5fe443544436a90c498e
giovane <giovanehl2@gmail.com>
2020-07-17 19:04:14 -0300

    ajustes

---
+++ TestAnalyser/Model/Alternativa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TestAnalyser.Model
+{
+    [Table("Alternativas")]
+    public class Alternativa
+    {
+        public Alternativa()
+        {
+            Questao =  new Questao();
+        }
+        [Key]
+        public int AlternativaId { get; set; }
+        public string DescAlternativa { get; set; }
+        public int correto { get; set; }
+        public Questao Questao { get; set; }
+
+    }
+}
